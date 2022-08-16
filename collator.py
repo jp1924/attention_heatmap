108635f3840cebe9b5a25425882ca7e6532d5479
jp1924 <jp42maru@gmail.com>
2022-08-16 15:08:00 +0900

    Add: Collator

---
+++ collator.py
@@ -0,0 +1,24 @@
+from typing import Dict, List, Union
+import transformers
+import torch
+from kobert_tokenizer import KoBertTokenizer
+
+
+class BertCollator:
+    def __init__(self, tokenizer: KoBertTokenizer):
+        self.tokenizer = tokenizer
+        pass
+
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        batch = dict()
+        input_ids = [{"input_ids": feature["input_ids"]} for feature in features]
+        batch = self.tokenizer.pad(
+            encoded_inputs=input_ids, padding=True, return_attention_mask=True, return_tensors="pt"
+        )
+
+        label = [feature["label"] for feature in features]
+        batch["labels"] = torch.tensor(label)
+
+        return batch
