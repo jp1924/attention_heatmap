b9ad501a07463446dd78d4ac0c95f57220392874
jp1924 <jp42maru@gmail.com>
2022-08-22 15:24:30 +0900

    Clean: change name

--- argument.py
+++ argument.py
@@ -1,9 +1,7 @@
 from transformers import TrainingArguments
 from dataclasses import dataclass, field
-import os
+
 
 @dataclass
-class StudyForTrainingArguments(TrainingArguments):
-    cache_dir: str = field(
-        default=None
-    )
+class CustomTrainingArguments(TrainingArguments):
+    cache_dir: str = field(default=None)
