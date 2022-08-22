5d0cb8ca7434ab916ef5ff9ba34a722be52a4099
jp1924 <jp42maru@gmail.com>
2022-08-22 15:16:27 +0900

    Add: Metrics

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import datasets
-from kobert_tokenizer import KoBertTokenizer
-from transformers import HfArgumentParser, Trainer, BertForSequenceClassification, BatchEncoding
-from check_model import CheckForBertForSequenceClassification
-from argument import StudyForTrainingArguments
-from setproctitle import setproctitle
-from integrations import CustomWansbCallBack
-from collator import BertCollator
 import os
+from typing import Dict, List, Union
+
+import datasets
+import torch
+from setproctitle import setproctitle
+from transformers import BatchEncoding, BertConfig, BertForSequenceClassification, HfArgumentParser
+
+from argument import StudyForTrainingArguments
+from collator import BertCollator
+from integrations import CustomWansbCallBack
+from kobert_tokenizer import KoBertTokenizer
+from trainer import CustomTrainer
 
 
-def main(args):
-
+def main(args) -> None:
     my_cache_dir = r"/data/jsb193/[study]Attention/.cache"
 
     NSMC_datasets = datasets.load_dataset("nsmc", cache_dir=my_cache_dir)
     bert_tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert", cache_dir=my_cache_dir)
-    model = CheckForBertForSequenceClassification.from_pretrained(
-        "monologg/kobert", cache_dir=my_cache_dir
-    )
+    config = BertConfig(vocab_size=bert_tokenizer.vocab_size, num_labels=1)
+    model = BertForSequenceClassification.from_pretrained("monologg/kobert", cache_dir=my_cache_dir, config=config)
+
+    model = model.to("cpu")
 
     def preprocess(input_data: datasets.Dataset) -> BatchEncoding:
         input_text = input_data["document"]
@@ -29,14 +33,24 @@ def main(args):
 
     NSMC_train = NSMC_datasets["train"].map(preprocess, num_proc=10)
     NSMC_test = NSMC_datasets["test"].map(preprocess, num_proc=10)
+    callback_func = [CustomWansbCallBack] if os.getenv("WANDB_DISABLED") != "true" else None
+    accuracy = datasets.load_metric("accuracy")
 
-    trainer = Trainer(
+    def metrics(outputs: Dict[str, Union[List[int], torch.Tensor]]) -> Dict[str, int]:
+        predictions = outputs.predictions
+        references = outputs.label_ids
+        result = accuracy._compute(predictions, references, normalize=False)
+        return {"Accuracy": result}
+
+    trainer = CustomTrainer(
         model=model,
         args=args,
+        compute_metrics=metrics,
         train_dataset=NSMC_train,
         eval_dataset=NSMC_test,
         data_collator=collator,
-        callbacks=[CustomWansbCallBack],
+        callbacks=callback_func,
+        tokenizer=bert_tokenizer,
     )
 
     trainer.train()
@@ -45,7 +59,8 @@ def main(args):
 if "__main__" == __name__:
     os.environ["WANDB_PROJECT"] = "[study]attention_machanism"
     os.environ["CODE_DIR"] = r"/home/jsb193/workspace/[study]Attention/Attention_Mechanism"
-    setproctitle("[JP]study_for_attention")
+    os.environ["WANDB_DISABLED"] = "false"
+    setproctitle("[JP]attention")
     parser = HfArgumentParser(StudyForTrainingArguments)
     training_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
     main(training_args)
