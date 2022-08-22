e5109aa8dbfe64f0ea643664fcb51ca31c34a78d
jp1924 <jp42maru@gmail.com>
2022-08-22 15:16:03 +0900

    Add: label int to float

--- collator.py
+++ collator.py
@@ -5,20 +5,16 @@ from kobert_tokenizer import KoBertTokenizer
 
 
 class BertCollator:
-    def __init__(self, tokenizer: KoBertTokenizer):
+    def __init__(self, tokenizer: KoBertTokenizer) -> None:
         self.tokenizer = tokenizer
-        pass
 
-    def __call__(
-        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
-    ) -> Dict[str, torch.Tensor]:
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         batch = dict()
         input_ids = [{"input_ids": feature["input_ids"]} for feature in features]
-        batch = self.tokenizer.pad(
-            encoded_inputs=input_ids, padding=True, return_attention_mask=True, return_tensors="pt"
-        )
+        batch = self.tokenizer.pad(encoded_inputs=input_ids, padding=True, return_attention_mask=True, return_tensors="pt")
 
         label = [feature["label"] for feature in features]
-        batch["labels"] = torch.tensor(label)
+        batch["labels"] = torch.tensor(label).to(torch.float32)
+        batch["output_attentions"] = True
 
         return batch
