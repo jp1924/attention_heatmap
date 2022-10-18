492c64867a09af3e80f843561df7b8bbb037c96f
jp1924 <jp42maru@gmail.com>
2022-10-18 11:02:50 +0900

    Update: add script variable

--- script/run_main.sh
+++ script/run_main.sh
@@ -1,22 +1,22 @@
+script_path = ""
+output_dir = ""
+cahce = ""
 
 
 
-
-
-
-python main.py \
-    --output_dir "/home/jsb193/workspace/[study]Attention/output_dir" \
-    --cache_dir "/data/jsb193/[study]Attention/.cache" \
-    --learning_rate "1e-5" \
-    --per_device_train_batch_size "4" \
-    --per_device_eval_batch_size "4" \
-    --num_train_epochs "3" \
-    --gradient_accumulation_steps "2" \
-    --eval_accumulation_steps "2" \
-    --lr_scheduler_type "linear" \
-    --warmup_steps "1000" \
-    --logging_strategy "steps" \
-    --logging_steps "10" \
-    --evaluation_strategy "steps" \
-    --eval_steps "5000" \
+python $script_path \
+    --output_dir $output_dir \
+    --cache_dir $cahce \
+    --learning_rate "" \
+    --per_device_train_batch_size "" \
+    --per_device_eval_batch_size "" \
+    --num_train_epochs "" \
+    --gradient_accumulation_steps "" \
+    --eval_accumulation_steps "" \
+    --lr_scheduler_type "" \
+    --warmup_steps "" \
+    --logging_strategy "" \
+    --logging_steps "" \
+    --evaluation_strategy "" \
+    --eval_steps ""
 
