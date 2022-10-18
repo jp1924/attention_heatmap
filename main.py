d287b9e01240634d5783d9bfed3ca38d831f3b4a
jp1924 <jp42maru@gmail.com>
2022-10-18 09:07:24 +0900

    Clean: Renaming and modularity

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import os
+from argparse import Namespace
 from typing import Dict, List, Union
 
 import datasets
 import torch
-from collator import BertCollator
+from evaluate import load
 from setproctitle import setproctitle
-from trainer import CustomTrainer
-from transformers import BatchEncoding, BertConfig, BertForSequenceClassification, HfArgumentParser
+from transformers import (
+    BatchEncoding,
+    BertConfig,
+    BertForSequenceClassification,
+    BertTokenizerFast,
+    HfArgumentParser,
+    Trainer,
+)
 from transformers.integrations import WandbCallback
 
-from utils import BertDataArguments, BertModelArguments, BertTrainingArguments
+from data import BertHeatmapCollator
+from trainer import HeatmapTrainer
+from utils import BertModelArguments, BertTrainingArguments
 
 
 def main(parser: HfArgumentParser) -> None:
-    train_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
-    setproctitle("[JP]test")  # [TODO]: trainginarguemnt의 run_name으로 변경
+    train_args, model_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
+    setproctitle(train_args.run_name)  # [TODO]: trainginarguemnt의 run_name으로 변경
 
     def metrics(outputs: Dict[str, Union[List[int], torch.Tensor]]) -> Dict[str, int]:
+        """_summary_
+
+        Args:
+            outputs (Dict[str, Union[List[int], torch.Tensor]]): _description_
+
+        Returns:
+            Dict[str, int]: _description_
+        """
         predictions = outputs.predictions
         predictions = predictions.argmax(-1)
         references = outputs.label_ids
@@ -24,46 +41,85 @@ def main(parser: HfArgumentParser) -> None:
         return result
 
     def preprocess(input_data: datasets.Dataset) -> BatchEncoding:
+        """_summary_
+
+        Args:
+            input_data (datasets.Dataset): _description_
+
+        Returns:
+            BatchEncoding: _description_
+        """
         input_text = input_data["document"]
-        output_data = bert_tokenizer(input_text, return_attention_mask=False)
+        output_data = tokenizer(input_text, return_attention_mask=False)
         output_data["label"] = input_data["label"]
         return output_data
 
-    # [TODO]: 모델 monologg/kobert에서 klue-bert로 변경
-    bert_tokenizer = KoBertTokenizer.from_pretrained("monologg/kobert", cache_dir=train_args.cache_dir)
-    config = BertConfig(vocab_size=bert_tokenizer.vocab_size, num_labels=2)  # [TODO]: num_labels argument로 빼기
+    tokenizer = BertTokenizerFast.from_pretrained(model_args.model_name, cache_dir=train_args.cache)
+    config = BertConfig(vocab_size=tokenizer.vocab_size, num_labels=model_args.num_labels)
     model = BertForSequenceClassification.from_pretrained(
-        "monologg/kobert", cache_dir=train_args.cache_dir, config=config
+        model_args.model_name, cache_dir=train_args.cache, config=config
     )
 
-    NSMC_datasets = datasets.load_dataset("nsmc", cache_dir=train_args.cache_dir)  # [TODO]: data이름도 별도 데이터로 변경
-    NSMC_train = NSMC_datasets["train"].map(preprocess, num_proc=10)  # [TODO]: num_proc 별도 args로 변경
-    NSMC_test = NSMC_datasets["test"].map(preprocess, num_proc=10)
+    NSMC_datasets = datasets.load_dataset("nsmc", cache_dir=train_args.cache)
+    NSMC_train = NSMC_datasets["train"].map(preprocess, num_proc=train_args.num_proc)
+    NSMC_valid = NSMC_datasets["test"].map(preprocess, num_proc=train_args.num_proc)
 
-    accuracy = datasets.load_metric("accuracy")  # [TODO]: evaluate로 변경
+    accuracy = load("accuracy")
     callback_func = [WandbCallback] if os.getenv("WANDB_DISABLED") != "true" else None
-    collator = BertCollator(tokenizer=bert_tokenizer)
+    collator = BertHeatmapCollator(tokenizer)
 
-    trainer = CustomTrainer(
+    trainer = HeatmapTrainer(
         model=model,
         args=train_args,
         compute_metrics=metrics,
         train_dataset=NSMC_train,
-        eval_dataset=NSMC_test,
+        eval_dataset=NSMC_valid,
         data_collator=collator,
         callbacks=callback_func,
-        tokenizer=bert_tokenizer,
+        tokenizer=tokenizer,
     )
     # do train, do eval, do predict으로 변경
     trainer.train(ignore_keys_for_eval=["attentions"])
+    if train_args.do_train:
+        train(trainer, train_args)
+    if train_args.do_eval:
+        eval(trainer, NSMC_valid)
+    if train_args.do_predict:
+        predict(trainer, NSMC_valid)
+
+
+def train(trainer: Trainer, args: Namespace) -> None:
+    """_summary_
+
+    Args:
+        trainer (Trainer): _description_
+        args (Namespace): _description_
+    """
+    trainer
+
+
+def eval(trainer: Trainer, valid_data: datasets.Dataset) -> None:
+    """_summary_
+
+    Args:
+        trainer (Trainer): _description_
+        valid_data (datasets.Dataset): _description_
+    """
+    trainer
+
+
+def predict(trainer: Trainer, test_data: datasets.Dataset) -> None:
+    """_summary_
+
+    Args:
+        trainer (Trainer): _description_
+        test_data (datasets.Dataset): _description_
+    """
+    trainer
 
 
 if "__main__" == __name__:
     # [TODO]: 경로 관련 설정 삭제, 환경 변수 launch.json, shell script로 변경
-    os.environ["WANDB_PROJECT"] = "[study]attention_machanism"
-    os.environ["CODE_DIR"] = r"/home/jsb193/workspace/[study]Attention/Attention_Mechanism"
-    # os.environ["WANDB_DISABLED"] = "false"
-
-    parser = HfArgumentParser(CustomTrainingArguments)
+    parser = HfArgumentParser(BertTrainingArguments, BertModelArguments)
 
     main(parser)
