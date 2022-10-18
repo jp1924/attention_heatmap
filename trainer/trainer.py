2b8140e2b032f412d083195104d3bbb6816e2451
jp1924 <jp42maru@gmail.com>
2022-10-18 11:02:09 +0900

    Clean: add docstring & add heatmap func

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -16,23 +16,18 @@ if is_apex_available():
 
 
 class HeatmapTrainer(Trainer):
+    heatmap_print: bool = False
+
     def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
-        """
-        Perform a training step on a batch of inputs.
-
-        Subclass and override to inject custom behavior.
-
+        """Heatmap을 표시하기 위한 Trainer 입니다.
+            heatmap을 print하기 위해선 heatmap_print을 True로 설정해야 합니다.
+            heatmap은 deprecated됩니다! 그렇기 때문에 구 버전의 wandb를 사용해야 합니다.
         Args:
-            model (`nn.Module`):
-                The model to train.
-            inputs (`Dict[str, Union[torch.Tensor, Any]]`):
-                The inputs and targets of the model.
+            model (nn.Module): Trainer로 부터 model을 전달받습니다.
+            inputs (Dict[str, Union[torch.Tensor, Any]]): collator에 처리가 끝난 데이터를 전달받습니다.
 
-                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-                argument `labels`. Check your model's documentation for all accepted arguments.
-
-        Return:
-            `torch.Tensor`: The tensor with training loss on this batch.
+        Returns:
+            torch.Tensor: loss값을 Trainer로 반환합니다.
         """
         model.train()
         inputs = self._prepare_inputs(inputs)
@@ -44,40 +39,8 @@ class HeatmapTrainer(Trainer):
         with self.compute_loss_context_manager():
             loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
 
-        if (self.state.global_step % self.args.eval_steps == 0) and (
-            self.state.is_world_process_zero and os.getenv("WANDB_DISABLED") != "true"
-        ):
-            with torch.no_grad():
-                label_sentence = "발연기 도저히 못보겠다 진짜 이렇게 연기를 못할거라곤 상상도 못했네"
-                cls_token = self.tokenizer._cls_token
-                sep_token = self.tokenizer._sep_token
-                test_sentence = self.tokenizer(label_sentence, return_attention_mask=True)
-
-                label_sentence = self.tokenizer.tokenize(label_sentence)
-
-                sentence = f"{cls_token} {label_sentence} {sep_token}".split(" ")
-                output_for_map = self.model.bert(
-                    torch.tensor([test_sentence["input_ids"]], device=loss.device),
-                    attention_mask=torch.tensor([test_sentence["attention_mask"]], device=loss.device),
-                    token_type_ids=None,
-                    position_ids=None,
-                    head_mask=None,
-                    inputs_embeds=None,
-                    output_attentions=True,
-                    output_hidden_states=True,
-                    return_dict=True,
-                )
-                attention_probs = output_for_map.attentions[11]
-                attention_first_head = attention_probs[0][0]
-
-                heatmap = wandb.plots.HeatMap(
-                    sentence,
-                    sentence,
-                    attention_first_head.cpu(),
-                    show_text=False,
-                )
-
-                wandb.log({f"attention_heatmap_{self.state.global_step}": heatmap})
+            if self.heatmap_print and os.getenv("WANDB_DISABLED") != "true":
+                self.print_heatmap(loss)
 
         if self.args.n_gpu > 1:
             loss = loss.mean()  # mean() to average on multi-gpu parallel training
@@ -98,3 +61,47 @@ class HeatmapTrainer(Trainer):
             loss.backward()
 
         return loss.detach()
+
+    def print_heatmap(self, loss: torch.Tensor) -> None:
+        """Heatmap을 wandb에 출력합니다.
+
+        Args:
+            loss (torch.Tensor): 모델의 device를 설정하기 위해 loss를 입력합니다.
+        """
+
+        step_caculate = self.state.global_step % self.args.eval_steps == 0
+        processer_check = self.state.is_world_process_zero
+
+        if step_caculate and processer_check:
+
+            label_sentence = "발연기 도저히 못보겠다 진짜 이렇게 연기를 못할거라곤 상상도 못했네"
+            cls_token = self.tokenizer._cls_token
+            sep_token = self.tokenizer._sep_token
+
+            with torch.no_grad():
+                test_sentence = self.tokenizer(label_sentence, return_attention_mask=True)
+                label_sentence = self.tokenizer.tokenize(label_sentence)
+
+                output_for_map = self.model.bert(
+                    torch.tensor([test_sentence["input_ids"]], device=loss.device),
+                    attention_mask=torch.tensor([test_sentence["attention_mask"]], device=loss.device),
+                    token_type_ids=None,
+                    position_ids=None,
+                    head_mask=None,
+                    inputs_embeds=None,
+                    output_attentions=True,
+                    output_hidden_states=True,
+                    return_dict=True,
+                )
+                attention_probs = output_for_map.attentions[11]
+                attention_first_head = attention_probs[0][0]
+
+                sentence = f"{cls_token} {label_sentence} {sep_token}".split(" ")
+                heatmap = wandb.plots.HeatMap(
+                    sentence,
+                    sentence,
+                    attention_first_head.cpu(),
+                    show_text=False,
+                )
+
+                wandb.log({f"attention_heatmap_{self.state.global_step}": heatmap})
