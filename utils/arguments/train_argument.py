b9e0b6bd219aaed37ab76bb5102cf902c9253cc3
jp1924 <jp42maru@gmail.com>
2022-10-19 17:18:24 +0900

    Add: hyperparameter search args

--- utils/arguments/train_argument.py
+++ utils/arguments/train_argument.py
@@ -1,5 +1,6 @@
 from transformers import TrainingArguments
 from dataclasses import dataclass, field
+from typing import Literal
 import os
 
 
@@ -16,4 +17,20 @@ class BertTrainingArguments(TrainingArguments):
     hyperparameter_search: bool = field(
         default=False,
         metadata="huggingface에서 제공하는 hyperparameter_search기능을 사용할지 말지에 대한 결정을 bool값으로 결정합니다.",
+    )
+    hp_trial: int = field(
+        default=None,
+        metadata="hyperparameter_search시 실행할 시험의 수를 설정합니다.",
+    )
+    hp_config: os.PathLike = field(
+        default=None,
+        metadata="hyperparameter_searcht설정 값을 담고 있는 json파일의 경로를 할당할 수 있습니다. json",
+    )
+    hp_backend: Literal["optuna", "ray", "wandb", "sigopt"] = field(
+        default=None,
+        metadata="hp를 시도할 플랫폼을 선택합니다. 플랫폼은 huggingface에서 지원하는 플랫폼만 가능합니다.",
+    )
+    hp_direction: Literal["minimize", "maximize"] = field(
+        default=None,
+        metadata="매트릭 값이 어느 방향으로 수렴할지를 결정합니다.",
     )
