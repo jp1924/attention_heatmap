e9e77715587d9555332737f1d19f96e841fba63e
jp1924 <jp42maru@gmail.com>
2022-10-19 10:34:12 +0900

    Add: help script

--- utils/arguments/model_argument.py
+++ utils/arguments/model_argument.py
@@ -3,4 +3,7 @@ from dataclasses import dataclass, field
 
 @dataclass
 class BertModelArguments:
-    num_labels: int = field(default=2)
+    num_labels: int = field(
+        default=2,
+        metadata="classification에 사용될 labels의 개수를 설정합니다. 만약 multi-classification인데 num_labels가 binary라면 학습이 안될 수 있습니다.",
+    )
--- utils/arguments/train_argument.py
+++ utils/arguments/train_argument.py
@@ -1,8 +1,19 @@
 from transformers import TrainingArguments
 from dataclasses import dataclass, field
+import os
 
 
 @dataclass
 class BertTrainingArguments(TrainingArguments):
-    cache: str = field(default=None)
-    num_proc: int = field(default=None)
+    cache: os.PathLike = field(
+        default=None,
+        metadata="cache_dir의 경로를 입력합니다.",
+    )
+    num_proc: int = field(
+        default=None,
+        metadata="전처리에 사용될 프로세서의 수를 결정합니다.",
+    )
+    hyperparameter_search: bool = field(
+        default=False,
+        metadata="huggingface에서 제공하는 hyperparameter_search기능을 사용할지 말지에 대한 결정을 bool값으로 결정합니다.",
+    )
