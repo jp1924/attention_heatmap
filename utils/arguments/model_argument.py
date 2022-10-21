6cf072d1d29283c7517d82d30921ffe2bf2f3d55
jp1924 <jp42maru@gmail.com>
2022-10-21 09:46:04 +0900

    Add: model_name args

--- utils/arguments/model_argument.py
+++ utils/arguments/model_argument.py
@@ -5,5 +5,11 @@ from dataclasses import dataclass, field
 class BertModelArguments:
     num_labels: int = field(
         default=2,
-        metadata="classification에 사용될 labels의 개수를 설정합니다. 만약 multi-classification인데 num_labels가 binary라면 학습이 안될 수 있습니다.",
+        metadata={
+            "help": "classification에 사용될 labels의 개수를 설정합니다. 만약 multi-classification인데 num_labels가 binary라면 학습이 안될 수 있습니다."
+        },
+    )
+    model_name: str = field(
+        default=None,
+        metadata={"help": "huggingface허브, 로컬로 부터 불러올 데이터의 경로를 입력합니다."},
     )
